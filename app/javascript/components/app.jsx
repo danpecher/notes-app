93d202f3050e1b8220afb8b6013c872e116dfc0d
Daniel Pecher <dan.pecher@gmail.com>
2018-01-17 20:21:31 -0800

    setup css modules

--- app/javascript/components/app.jsx
+++ app/javascript/components/app.jsx
@@ -1,10 +1,11 @@
 import React, {Component} from 'react'
 import Editor from './editor.jsx'
 import Notes from './notes.jsx'
+import styles from './app.css'
 
 class App extends Component {
     render() {
-        return <div>
+        return <div className={styles.container}>
             <Notes />
             <Editor />
         </div>
